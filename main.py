03ee8c7cad4ae19f393ce3d8edd10f0a662e0640
Gustavo Rolo <gustavohenriquesantos2001@hotmail.com>
2023-02-06 22:07:10 -0300

    primeira-aula

---
+++ main.py
@@ -0,0 +1,15 @@
+# Importando bibioteca pandas
+
+import pandas as pd 
+
+
+"""
+
+Curso de Extensão : Análise dados na prática com Python Pandas
+
+Data : 06-02-2023       Autor: Gustavo Rolo       RA: 32111822
+
+"""
+
+
+combustiveis_df = pd.read_csv("precos-semestrais-ca-2022-01.csv")
